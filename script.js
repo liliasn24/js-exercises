ce2cc533cf5a7bfaabce951cb8143d4dde3dc853
= <salaslilia00@gmail.com>
2022-01-24 22:33:07 -0500

    arrow function and function declaration

---
+++ script.js
@@ -0,0 +1,16 @@
+//Functions - arrow
+
+const sum = () => {
+  const result = 5 + 5;
+  console.log(result);
+};
+
+sum();
+
+//Functions - declaration
+
+function dec(a, b) {
+  const res = a + b;
+  console.log(res);
+}
+dec(5, 3);
