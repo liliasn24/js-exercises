374f302656f0c1f2219484e524bcf9e9966c7ebb
= <salaslilia00@gmail.com>
2022-01-25 22:23:43 -0500

    function

--- script.js
+++ script.js
@@ -15,6 +15,16 @@ function dec(a, b) {
 }
 dec(5, 3);
 
+function computeReminder(a, b) {
+  if (b === 0) {
+    console.log("Infinity");
+  } else {
+    return a % b;
+  }
+}
+
+computeReminder(10, 2);
+
 //Loops
 
 for (let number = 10; number <= 50; number++) {
@@ -31,6 +41,6 @@ let number1 = 0;
 let number2 = 0;
 
 while (number1 < 3) {
-  number++;
+  number1++;
   console.log(number1);
 }
