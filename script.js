4b1d569808d01d88e293b0c368738d964d818156
= <salaslilia00@gmail.com>
2022-01-25 22:19:56 -0500

    types of loops

--- script.js
+++ script.js
@@ -20,3 +20,17 @@ dec(5, 3);
 for (let number = 10; number <= 50; number++) {
   console.log(number);
 }
+
+let i = 0;
+do {
+  i += 1;
+  console.log(i);
+} while (i < 10);
+
+let number1 = 0;
+let number2 = 0;
+
+while (number1 < 3) {
+  number++;
+  console.log(number1);
+}
