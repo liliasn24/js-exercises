b6f7c6531f74513b9b95ffe5bff7bdfccbc11633
= <salaslilia00@gmail.com>
2022-07-24 22:56:40 -0400

    module operator

--- script.js
+++ script.js
@@ -199,3 +199,12 @@ function findPerimeter(length, width) {
 }
 
 findPerimeter(6, 7);
+
+function remainder(x, y) {
+  return x % y;
+}
+
+remainder(1, 3);
+remainder(3, 4);
+remainder(-9, 45);
+remainder(5, 5);
