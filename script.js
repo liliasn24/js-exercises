00fcc086d6289a59026cc30a51a160dfa37b5dff
= <salaslilia00@gmail.com>
2022-07-27 21:37:13 -0400

    polygon function

--- script.js
+++ script.js
@@ -274,3 +274,8 @@ boolToString(true)
 // The formula (n - 2) x 180 gives the sum of all the measures of the angles of an n-sided polygon.
 
 
+// Given an n-sided regular polygon n, return the total sum of internal angles (in degrees).
+
+function sumPolygon(n) {
+	return (n - 2) * 180;
+}
