962c6e3d9a610b3e772b4fefb6ee9bb273307898
= <salaslilia00@gmail.com>
2022-08-03 22:22:16 -0400

    Math.min and Math.max

--- script.js
+++ script.js
@@ -306,3 +306,11 @@ function calculator(str) {
 }
 
 calculator("23+4")
+
+
+function minMax(arr) {
+	return [Math.min(...arr), Math.max(...arr)]  //note the coma to get the correct result 
+}
+minMax([1, 2, 3, 4, 5])  // [1, 5]
+minMax([2334454, 5])   // [5, 2334454]
+minMax([1])   // [1, 1]
