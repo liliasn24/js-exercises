273120248260c5db3a88599f95a27d3c9cbcf5a6
= <salaslilia00@gmail.com>
2022-11-24 10:46:23 -0500

    swithc case with example

--- script.js
+++ script.js
@@ -671,5 +671,38 @@ relationToLuke("Leia")
 relationToLuke("Han")
 relationToLuke("R2D2")
 
+//SWITCH CASE 
 
+function getContainer(product) {
+	let container
+  switch (product) {
+		case "Bread":
+			container = "bag";
+			break;
+		case "Beer":
+		case "Milk":
+			container = "bottle"
+			break
+		case "Cereals":
+			container = "box"
+			break
+		case "Eggs":
+			container = "carton"
+			break
+		case "Candy":
+			container = "plastic";
+			break
+		default:
+			container = null
+	}
 
+	return container
+}
+
+getContainer("Bread")
+
+getContainer("Beer")
+
+getContainer("Candy")
+
+getContainer("Cheese")
